82a23e1df9baf4e299ff676748a97cb77d36c773
SaidLeon04 <1722110300@utectulancingo.edu.mx>
2024-04-05 08:18:26 -0600

    Update

--- dev/IA/models/generative.py
+++ dev/IA/models/generative.py
@@ -2,7 +2,7 @@ import mysql.connector
 import google.generativeai as genai
 
 # IA Stuff
-genai.configure(api_key="AIzaSyDLzvnrcjUvkXtmrei5AwPHLp6IucRmddI")
+genai.configure(api_key="")
 
 generation_config = {
   "temperature": 1,
